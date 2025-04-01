f7983b5d1731537f86f22606dbd27338d8ccf558
Linn Amondi <linnamondi@gmail.com>
2025-04-01 09:58:26 +0300

    initial commit

---
+++ assignment.js
@@ -0,0 +1,53 @@
+let message='the child walked to the market';
+let newmassage=message.charAt(0).toUpperCase()+ message.slice(1);
+console.log({newmassage});
+
+let word='The child walked to the market';
+let newword='The'+'f'+'child walked to the market'
+console.log({newword})
+
+let count ="hello world, hello JavaScript!";
+let newcount="hello";
+const total=count.split(newcount).length-1;
+console.log(total);
+
+
+let phrase='coding';
+let newphrase=phrase.slice(3,);
+console.log({newphrase})
+
+let number='twenty';
+let newnumber=number.slice(0,3);
+console.log({newnumber});
+
+
+let ascending='twenty';
+let newascending =ascending.split('').sort().join('');
+console.log({newascending});
+
+let love = "  my mum is the best   ";
+let newlove = love.split(" ").join("");
+console.log(newlove);
+
+let wordy='greeting';
+console.log(wordy.indexOf('t'));
+
+let invoice='kiswahili';
+let newinvoice=invoice.substring(3,9,invoice.length-1)
+console.log(newinvoice);
+
+
+
+let subject='mathematics';
+let newsubject=subject.substring(0,8,subject.length-1);
+console.log(newsubject);
+
+
+let lastTwoChar=subject.slice(0 , -2);
+console.log(lastTwoChar);
+
+let right='left'
+let newright=right.toUpperCase();
+console.log({newright});
+
+
